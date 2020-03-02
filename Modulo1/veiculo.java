c21ee761a2e8eb035690e4f8c3642945d34d97ec
Agnes Midori <54656640+agnesmidori@users.noreply.github.com>
2020-03-02 13:58:34 -0300

    Create veiculo.java
    Enunciado - Classe Veiculo

---
+++ Modulo1/veiculo.java
@@ -0,0 +1,15 @@
+public class Veiculo {
+	protected int velocidade;
+	
+	public void acelera (int incremento) {
+		velocidade = velocidade + incremento;
+	}
+	
+	public void freia (int decremento) {
+		velocidade = velocidade - decremento;
+	}
+	
+	public String toString () {
+		return new String ("Velocidade: " + velocidade);
+	}
+}
