3dd9ddcfebee957095528a6be807751b62347fbd
Agnes Midori <54656640+agnesmidori@users.noreply.github.com>
2020-03-06 13:38:58 -0300

    Update Main.java

--- Modulo1/Main.java
+++ Modulo1/Main.java
@@ -1,12 +1,13 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// Instanciando tipo Veiculo
+		// Instanciando a classe Onibus
 		Onibus onibus1 = new Onibus();
 		onibus1.embarca (10);
 		onibus1.acelera(40);
 		System.out.println(onibus1.toString());
 		
+		// Instanciando a classe Foguete
 		Foguete foguete1 = new Foguete (); 
 		foguete1.embarcaViagem();
 		System.out.println(foguete1.toString());
