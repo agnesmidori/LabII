d79ffa12011fe6ae0a3cd16d378520bf8ff09fc7
Agnes Midori <54656640+agnesmidori@users.noreply.github.com>
2020-03-06 14:32:48 -0300

    Update Main.java
    Acrescentando // Teste

--- Modulo1/Main.java
+++ Modulo1/Main.java
@@ -11,6 +11,15 @@ public class Main {
 		Foguete foguete1 = new Foguete (); 
 		foguete1.embarcaViagem();
 		System.out.println(foguete1.toString());
+		
+		// Teste 
+		Veiculo veiculo = new Onibus ();
+		veiculo.acelera(80);
+		((Onibus)veiculo).embarca(50);
+		System.out.println(veiculo.toString());		
+		((Onibus)veiculo).desembarca(30);
+		veiculo.freia(80);
+		System.out.println(veiculo.toString());		
 	
 	}
 }
