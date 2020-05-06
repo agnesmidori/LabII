430ce666d21770779e3a8432046f093917708759
Agnes Midori <54656640+agnesmidori@users.noreply.github.com>
2020-05-06 02:28:31 -0300

    Novo teste

--- Modulo8/ListTest.java
+++ Modulo8/ListTest.java
@@ -4,17 +4,18 @@ public class ListTest {
         // Testando metodo - public boolean troca_ter_quarto()
         List list = new List();  // create the List container
         // Populando a lista
-        list.insertAtFront(6);
+        list.insertAtFront(2);
         list.insertAtBack(2);
         list.insertAtBack(4);
         list.insertAtBack(3);
+        list.insertAtBack(5);
         list.print();
-        list.troca_ter_quarto();
+        boolean teste = list.troca_ter_quarto();
+        if (teste == true) System.out.println("True\n");
+        else if (teste == false) System.out.println("False\n");
         list.print();
         //Testando metodo - public boolean remove_impar()
         list.remove_impar();
         list.print();
-
     }
-
 }
